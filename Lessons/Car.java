1ecf23b91d64fb35b4b2f8c27dba03edd9732bf9
github username <github email address>
2019-10-29 10:45:52 -0400

    Arrays(Cars): add car to array

--- Lessons/Car.java
+++ Lessons/Car.java
@@ -6,8 +6,29 @@ public class Car {
         for(String car : carsArr)
             System.out.println(car);
     }
+    // Search for car in array - Returns the car that's found, or string "Car Not Found"
+
+    // Add car to array
+    public static void addCarToArray(String newCar) {
+        // Create a new cars array
+        String[] newCarsArray = new String[cars.length + 1];
+        // Assign the items in cars array to the new cars array
+        for (int i = 0; i < cars.length; i++)
+            newCarsArray[i] = cars[i];
+
+        // Now add the new car to the end of the new array
+        newCarsArray[newCarsArray.length - 1] = newCar;
+
+        // Point the cars array to the newCarsArray in memory
+        cars = newCarsArray;
+    }
+
+    // Remove car from  array (Will be made after "Search for car in array")
 
     public static void main(String[] args) {
+        printCars(cars);
+        System.out.println("-----\n     \n-----");
+        addCarToArray("Tesla Model X");
         printCars(cars);
     }
 }
